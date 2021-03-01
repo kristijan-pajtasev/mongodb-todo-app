e9fae012dde7260c9d778c362d81a7ad94f0c865
Kristijan <kristijan.pajtasev@hotmail.com>
2021-03-01 00:45:45 +0000

    updated mock file

--- components/ToDoList/ToDoContainer.js
+++ components/ToDoList/ToDoContainer.js
@@ -5,6 +5,11 @@ const toDos = [
         text: "hello world",
         isCompleted: true,
         id: 123
+    },
+    {
+        text: "hello world 2",
+        isCompleted: false,
+        id: 1234
     }
 ]
 

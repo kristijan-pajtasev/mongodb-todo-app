f34a65423d81cc255c311c9b5437a85b74873787
Kristijan <kristijan.pajtasev@hotmail.com>
2021-03-01 00:56:32 +0000

    fetch todos

--- components/ToDoList/ToDoContainer.js
+++ components/ToDoList/ToDoContainer.js
@@ -1,21 +1,18 @@
 import ToDoList from "./ToDoList";
+import {useEffect, useState} from "react";
 
-const toDos = [
-    {
-        text: "hello world",
-        isCompleted: true,
-        id: 123
-    },
-    {
-        text: "hello world 2",
-        isCompleted: false,
-        id: 1234
-    }
-]
 
 function ToDoContainer() {
+    const [todos, setToDos] = useState(null);
+    useEffect(() => {
+        fetch("/api/todo")
+            .then(res => res.json())
+            .then(setToDos)
+    }, [])
+
+    if(!todos) return <div>loading</div>
     return (
-        <ToDoList todos={toDos} />
+        <ToDoList todos={todos} />
     )
 }
 

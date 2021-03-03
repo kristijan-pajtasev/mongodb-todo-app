55bb965e01553625d7616e4217385ee4ca8105fb
Kristijan <kristijan.pajtasev@hotmail.com>
2021-03-03 00:05:47 +0000

    item cleanup

--- components/ToDoItem/ToDoItem.js
+++ components/ToDoItem/ToDoItem.js
@@ -4,7 +4,6 @@ function ToDoItem({text, id, isCompleted}) {
     return (
         <div className={styles.ToDoItem}>
             <div>{text}</div>
-            <div>{id}</div>
             <div>{isCompleted ? "completed" : "not completed"}</div>
         </div>
     )
